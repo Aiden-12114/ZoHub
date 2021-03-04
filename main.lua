5f3149e2fb5fd16c595169afaa61f84b741e891a
Aiden <aidensomboon07@gmail.com>
2021-03-04 17:22:57 +0800

    e

--- main.lua
+++ main.lua
@@ -1,5 +1,13 @@
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/GreenDeno/Venyx-UI-Library/main/source.lua"))()
-
 local ZoHub = library.new("ZoHub")
 
-print("Ready")
+local scriptsPage = ZoHub:addPage("Scripts")
+local scriptsSection = scriptsPage:addSection("")
+
+scriptsSection:addButton("Infinite Yield", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Aiden-12114/ZoHub/master/scripts/InfiniteYield.lua"))()
+end)
+
+scriptsSection:addButton("Reviz Admin", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Aiden-12114/ZoHub/master/scripts/Reviz.lua"))()
+end)
